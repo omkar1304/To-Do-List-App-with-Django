d9d4a6374bbf8046cad57ebd32a9e89aef1670f1
omkar1304 <omkarpedamkar2@gmail.com>
2022-04-20 20:58:29 +0530

    first commit

--- base/urls.py
+++ base/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate, TaskDelete, UserLoginView, UserLogoutView, UserRegisterView, TaskReorder
+from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate, TaskDelete, UserLoginView, UserLogoutView, user_register, TaskReorder
 
 urlpatterns = [
     path('login', UserLoginView.as_view(), name="login"),
     path('logout', UserLogoutView.as_view(), name="logout"),
-    path('register', UserRegisterView.as_view(), name='register'),
+    path('register', user_register, name='register'),
 
     path('', TaskList.as_view(), name="tasklist"),
     path('task-detail/<int:pk>/', TaskDetail.as_view(), name="task-detail"),
--- todo_list/settings.py
+++ todo_list/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -115,3 +116,8 @@ LOGIN_URL = 'login'
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR, 'static']
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+
+# Activate Django-Heroku
+django_heroku.settings(locals())
