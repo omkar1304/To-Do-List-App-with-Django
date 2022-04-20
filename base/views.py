1aee021b8be58752753f880998747133a96316df
omkar1304 <omkarpedamkar2@gmail.com>
2022-04-20 20:56:01 +0530

    first commit

--- base/views.py
+++ base/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import redirect
+from django.contrib import messages
+from django.contrib.auth.models import User
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView, FormView
@@ -14,7 +15,7 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Imports for Reordering Feature
 from django.views import View
-from django.shortcuts import redirect
+from django.shortcuts import redirect, render
 from django.db import transaction
 
 from .models import Task
@@ -34,22 +35,48 @@ class UserLogoutView(LogoutView):
     next_page = 'login'
 
 
-class UserRegisterView(FormView):
-    template_name = 'base/register.html'
-    form_class = UserCreationForm
-    success_url = reverse_lazy('tasklist')
-    redirect_authenticated_user = True
+# class UserRegisterView(FormView):
+#     template_name = 'base/register.html'
+#     form_class = UserCreationForm
+#     success_url = reverse_lazy('tasklist')
+#     redirect_authenticated_user = True
+#
+#     def form_valid(self, form):
+#         user = form.save()
+#         if user is not None:
+#             login(self.request, user)
+#         return super(UserRegisterView, self).form_valid(form)
+#
+#     def get(self, *args, **kwargs):
+#         if self.request.user.is_authenticated:
+#             return redirect('tasklist')
+#         return super(UserRegisterView, self).get(*args, **kwargs)
 
-    def form_valid(self, form):
-        user = form.save()
-        if user is not None:
-            login(self.request, user)
-        return super(UserRegisterView, self).form_valid(form)
+def user_register(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password1 = request.POST['password1']
+        password2 = request.POST['password2']
 
-    def get(self, *args, **kwargs):
-        if self.request.user.is_authenticated:
-            return redirect('tasklist')
-        return super(UserRegisterView, self).get(*args, **kwargs)
+        if password1 == password2:
+            if User.objects.filter(username=username).exists():
+                messages.info(request, "User Name Taken")
+                return redirect('register')
+
+            else:
+                user = User.objects.create_user(first_name='first_name', last_name='last_name', username=username,
+                                                password=password1, email=None)
+                user.save()
+                if user is not None:
+                    login(request, user)
+                return redirect('tasklist')
+        else:
+            messages.info(request, "Password Not Matching")
+            return redirect('register')
+
+    else:
+        return render(request, 'base/register.html')
+    return redirect('tasklist')
 
 
 class TaskList(LoginRequiredMixin, ListView):
@@ -102,6 +129,7 @@ class TaskDelete(LoginRequiredMixin, DeleteView):
     context_object_name = 'task'
     template_name = 'base/taskdelete.html'
     success_url = reverse_lazy('tasklist')
+
 
 class TaskReorder(View):
     def post(self, request):
